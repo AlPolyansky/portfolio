1eb4e44fa61a58015d500b8d8b7980caa3021e65
Eclerr <eclerrrr@yandex.ru>
2016-02-12 23:57:54 +0300

    Анмация, скролл, мобильная верся

--- js/common.js
+++ js/common.js
@@ -3,6 +3,22 @@ $(document).ready(function() {
 	var sideBar = $('.sideBar'),
 	wrapperWidth = $(".wrapper").width();
 
+	$(".popup_content").magnificPopup({
+		type: 'image',
+		closeOnContentClick: false,
+		closeBtnInside: true,
+		mainClass: 'mfp-with-zoom',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0,1]
+		},
+		zoom: {
+			enabled: true,
+			duration: 300, // don't foget to change the duration also in CSS
+		}
+	});
+
 
 
 	$(sideBar).css("height", $("body").height());
@@ -24,7 +40,7 @@ $(document).ready(function() {
 
 	});
 
-
+	$(".jobs_content a, .scroll_tabs_wrap a").mPageScroll2id({ scrollSpeed: 800 });
 
 	/*$(".top_menu ul li a").click(function(){
 		$(".sandwich").toggleClass("active");
@@ -43,7 +59,8 @@ $(document).ready(function() {
 	});*/
 
 	$(window).scroll(function() {
-		if(($(this).scrollTop() > 400 && $(window).width()) >= 768){
+		if(($(this).scrollTop() > 600 && $(window).width()) > 768){
+			$(".scroll_tabs_wrap").show("slow");
 			$(sideBar).css({
 				'margin-left': '-500px'
 				
@@ -52,7 +69,8 @@ $(document).ready(function() {
 				'padding-left': '0'
 			});
 		}
-		if(($(this).scrollTop() < 400 && $(window).width()) >= 768) {
+		if(($(this).scrollTop() < 600 && $(window).width()) > 768) {
+			$(".scroll_tabs_wrap").hide("slow");
 			$(sideBar).css({
 				'margin-left': '-250px'
 				
@@ -63,8 +81,9 @@ $(document).ready(function() {
 		}
 	})
 
+
 	$(window).resize(function(){
-		if (($(window).width()) <= 768) {
+		if (($(window).width()) <= 769) {
 			$(sideBar).css({
 				'margin-left': '-500px'
 				
@@ -87,3 +106,43 @@ $(document).ready(function() {
 		}
 	});
 });
+
+$(window).load(function() {
+	if($(this).width() > 768){
+			$(".sideBar").css({
+				'margin-left': '-500px'
+					
+			});
+			$(".content").css({
+				'padding-left': '0'
+			});
+		}
+	if($(this).width() <= 768){
+		$(".mfp-arrow").hide();
+		$(".sideBar").css({
+				'margin-left': '-500px'
+					
+			});
+			$(".content").css({
+				'padding-left': '0'
+			});
+	}
+	if($(this).scrollTop() == 0){
+		$(".sideBar").css({
+				'margin-left': '-250px'
+					
+			});
+		$(".content").css({
+				'padding-left': '250px'
+			});
+	if($(this).scrollTop() == 0 && $(this).width() <= 768){
+		$(".scroll_tabs_wrap").hide();
+		$(".sideBar").css({
+			'margin-left': '-500px'	
+		});
+		$(".content").css({
+			'padding-left': '0'
+		});
+	}
+	}
+}); 
