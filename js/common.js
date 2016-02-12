7855375d6799444d113af291956c8bbaffd69d9f
Eclerr <eclerrrr@yandex.ru>
2016-02-12 18:26:38 +0300

    Добавленна анмация

--- js/common.js
+++ js/common.js
@@ -8,19 +8,82 @@ $(document).ready(function() {
 	$(sideBar).css("height", $("body").height());
 	
 	$(".buttom_menu").click(function() {
+
   		$(".sandwich").toggleClass("active");
-  		$(".top_menu_wrap").toggleClass("open");
+
+  		if($(".top_menu div").hasClass("open")){
+  			$(".open").fadeOut(400,function(){
+  				$(".top_menu div").removeClass("open");
+  			})
+  		}
+  		else{
+  			$(".top_menu_wrap").addClass("open");
+  			$(".top_menu div").fadeIn(400);
+  		}
   		$(".top_menu ul").toggleClass("open_menu");
 
 	});
 
 
 
-	/*$(".top_mnu ul li a").click(function(){
-		$(".top_mnu").fadeOut(600);
+	/*$(".top_menu ul li a").click(function(){
 		$(".sandwich").toggleClass("active");
-		$(".top_text").css("opacity", "1");
-	});
-*/
 
+		if($(".top_menu div").hasClass("open")){
+  			$(".open").fadeOut(400,function(){
+  				$(".top_menu div").removeClass("open");
+  			})
+  		}
+  		else{
+  			$(".top_menu_wrap").addClass("open");
+  			$(".top_menu div").fadeIn(400);
+  		}
+  		$(".top_menu ul").toggleClass("open_menu");
+
+	});*/
+
+	$(window).scroll(function() {
+		if(($(this).scrollTop() > 400 && $(window).width()) >= 768){
+			$(sideBar).css({
+				'margin-left': '-500px'
+				
+			});
+			$(".content").css({
+				'padding-left': '0'
+			});
+		}
+		if(($(this).scrollTop() < 400 && $(window).width()) >= 768) {
+			$(sideBar).css({
+				'margin-left': '-250px'
+				
+			});
+			$(".content").css({
+				'padding-left': '250px'
+			});
+		}
+	})
+
+	$(window).resize(function(){
+		if (($(window).width()) <= 768) {
+			$(sideBar).css({
+				'margin-left': '-500px'
+				
+			});
+
+			$(".content").css({
+				'padding-left': '0'
+			});
+
+		}
+		else{
+			$(".top_menu_wrap").show();
+			$(sideBar).css({
+				'margin-left': '-250px'
+				
+			});
+			$(".content").css({
+				'padding-left': '250px'
+			});
+		}
+	});
 });
